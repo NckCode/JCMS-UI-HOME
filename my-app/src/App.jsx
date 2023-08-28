4716e278b73459bdeea263f71b460baca9eb9e60
Alisha Shaikh <Alisha.Shaikh@ril.com>
2023-08-28 15:27:46 +0530

    dsfsdf

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,17 @@
-import Card from "./Card"; // Adjust the path based on your project structure
-import "./App.css"; // You can add styling in a separate CSS file
+import Card from './Card' // Adjust the path based on your project structure
+import './App.css' // You can add styling in a separate CSS file
 
 const App = () => {
   return (
     <div className="app">
-      {/* this is free trial */}
+      {/* this is free totally trial */}
       <Card
         title="React Card Component"
         content="This is a simple card component in React."
       />
       <Card title="React Card 2" content="Another simple card component" />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
